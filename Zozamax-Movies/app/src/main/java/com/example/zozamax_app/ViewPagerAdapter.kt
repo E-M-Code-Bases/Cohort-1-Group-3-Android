c3c07c2a779777da7bb693570f49b90396bb39e1
otsimi <brightonobadia058@gmail.com>
2024-06-19 13:25:27 +0300

    tabLayout and SurfaceView

---
+++ Zozamax-Movies/app/src/main/java/com/example/zozamax_app/ViewPagerAdapter.kt
@@ -0,0 +1,15 @@
+package com.example.zozamax_app
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        TODO("Not yet implemented")
+    }
+}
