de3cc746c0a4b839e1f2f6a1b29ad5ad76696171
Ntialei <evelyntialei@gmail.com>
2024-06-21 14:42:31 +0300

    feat: added setOnClickListener for the toprated

--- Zozamax-Movies/app/src/main/java/com/example/zozamax_app/fragments/ListFragment.kt
+++ Zozamax-Movies/app/src/main/java/com/example/zozamax_app/fragments/ListFragment.kt
@@ -5,22 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.ui.AppBarConfiguration
 import com.example.zozamax_app.databinding.FragmentListBinding
 
+class ListFragment : Fragment() {
 
-class ListFragment: Fragment() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: FragmentListBinding
+
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         binding = FragmentListBinding.inflate(inflater, container, false)
 
+        binding.btnTopRated.setOnClickListener {
+        }
+
+        binding.btnPopular.setOnClickListener {
+        }
 
         return binding.root
     }
