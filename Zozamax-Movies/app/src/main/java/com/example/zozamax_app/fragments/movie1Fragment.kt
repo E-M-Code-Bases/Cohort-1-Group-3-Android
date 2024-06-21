cea4315833ab1bc04c2000076f9646f6db38dba2
Ntialei <evelyntialei@gmail.com>
2024-06-21 14:48:23 +0300

    feat:added the Movie1Fragment

---
+++ Zozamax-Movies/app/src/main/java/com/example/zozamax_app/fragments/movie1Fragment.kt
@@ -0,0 +1,48 @@
+package com.example.zozamax_app.fragments
+
+
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.MediaController
+import android.widget.VideoView
+import androidx.fragment.app.Fragment
+import com.example.zozamax_app.R
+import com.example.zozamax_app.databinding.FragmentMovie1Binding
+import com.example.zozamax_app.databinding.FragmentMovieBinding
+
+
+
+class Movie1Fragment : Fragment() {
+
+    private var _binding: FragmentMovie1Binding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMovie1Binding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Set up the VideoView
+        val videoView: VideoView = binding.movie1
+        val videoUri: Uri = Uri.parse("android.resource://" + requireActivity().packageName + "/" + R.raw.movie1)
+
+        videoView.setVideoURI(videoUri)
+        videoView.setMediaController(MediaController(requireContext()))
+        videoView.requestFocus()
+        videoView.start()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
