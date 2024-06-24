e2efc18e357a2863328ce25c67fee5d658e9c230
Priscahchela <chelangatprisca1@gmail.com>
2024-06-24 17:06:54 +0300

    fix: rename the class

--- Zozamax-Movies/app/src/main/java/com/example/zozamax_app/repository/PopularMovierepo.kt
+++ Zozamax-Movies/app/src/main/java/com/example/zozamax_app/repository/PopularMovierepo.kt
@@ -4,9 +4,9 @@ import com.example.zozamax_app.AppModule
 import com.example.zozamax_app.data.MovieApiResponse
 import retrofit2.Response
 
-class PopularMovierepo() {
-    suspend fun getPopulaMovies(): Response<MovieApiResponse> {
-   val retrofit = AppModule().getRetrofitInstance()
+class PopularMovieRepo() {
+    suspend fun getPopularMovies(): Response<MovieApiResponse> {
+        val retrofit = AppModule().getRetrofitInstance()
         return retrofit.getPopularMovies()
     }
 }
