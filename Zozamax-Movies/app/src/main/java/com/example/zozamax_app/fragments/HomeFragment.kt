5ff02a95c46128434861e2390a43a2ed6cd88cfd
okituibrian <okituibrian@gmail.com>
2024-06-25 12:51:17 +0300

    the drawer and its navigation from fragment layout

--- Zozamax-Movies/app/src/main/java/com/example/zozamax_app/fragments/HomeFragment.kt
+++ Zozamax-Movies/app/src/main/java/com/example/zozamax_app/fragments/HomeFragment.kt
@@ -1,82 +1,67 @@
 package com.example.zozamax_app.fragments
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView.HORIZONTAL
-import androidx.recyclerview.widget.RecyclerView.Orientation
-import androidx.recyclerview.widget.RecyclerView.VERTICAL
+import com.example.zozamax_app.R
 import com.example.zozamax_app.adapters.MovieAdapter
 import com.example.zozamax_app.adapters.items
 import com.example.zozamax_app.databinding.FragmentHomeBinding
 import com.example.zozamax_app.repository.PopularMovieRepo
 import com.example.zozamax_app.viewmodel.PopularModelProvider
 import com.example.zozamax_app.viewmodel.PopularViewModel
-import com.skydoves.transformationlayout.onTransformationStartContainer
-import kotlin.math.log
-
-private const val TAG = "popularMovies"
+import com.google.android.material.navigation.NavigationView
 
 class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        onTransformationStartContainer()
     }
 
-    @SuppressLint("SuspiciousIndentation")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         binding = FragmentHomeBinding.inflate(inflater, container, false)
 
-        val repo = PopularMovieRepo()
+        drawerLayout = binding.drawerLayout
+        navigationView = binding.navView
 
+        val repo = PopularMovieRepo()
         val productViewModel: PopularViewModel by viewModels {
             PopularModelProvider(repo)
         }
 
         productViewModel.popularMovies.observe(viewLifecycleOwner, Observer { movies ->
             // Update UI with the list of movies
-            Log.d(TAG, "popular movies -> $movies")
         })
-
-        val trailerFragment = TrailerFragment()
-        childFragmentManager.beginTransaction().apply {
-            replace(binding.trailer.id, trailerFragment)
-            addToBackStack(null)
-            commit()
-        }
-
-        val listFragment = ListFragment()
-        childFragmentManager.beginTransaction().apply {
-            replace(binding.list.id, listFragment)
-            addToBackStack(null)
-            commit()
-        }
 
         val movies = items().listMovies
 
-        // initialising the recycler view
+        // Initializing the recycler view
         binding.recView.apply {
-           // layoutManager = LinearLayoutManager(requireContext(), VERTICAL, false)
             layoutManager = GridLayoutManager(requireContext(), 2)
             adapter = MovieAdapter(movies, requireContext())
             setHasFixedSize(true)
         }
+
+        // Set up NavigationView with NavController
+        val navController = findNavController()
+        NavigationUI.setupWithNavController(navigationView, navController)
 
         return binding.root
     }
