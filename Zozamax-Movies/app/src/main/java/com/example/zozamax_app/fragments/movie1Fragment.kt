69c0b23ef3b9606f6280112183bd5ae823513770
CharlesMuvaka <muvakacharles@gmail.com>
2024-06-28 14:48:01 +0300

    feat: add a click listener to open the drawer .

--- Zozamax-Movies/app/src/main/java/com/example/zozamax_app/fragments/movie1Fragment.kt
+++ Zozamax-Movies/app/src/main/java/com/example/zozamax_app/fragments/movie1Fragment.kt
@@ -8,7 +8,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.MediaController
 import android.widget.VideoView
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.zozamax_app.R
 import com.example.zozamax_app.databinding.FragmentMovie1Binding
 
@@ -23,6 +26,13 @@ class Movie1Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentMovie1Binding.inflate(inflater, container, false)
+
+        val navHostFragment = childFragmentManager.findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
+        binding.navView.bringToFront()
+        binding.navView.setupWithNavController(navHostFragment.navController)
+        binding.image.setOnClickListener{
+            binding.drawerNav.openDrawer(GravityCompat.START)
+        }
 
 
         return binding.root
