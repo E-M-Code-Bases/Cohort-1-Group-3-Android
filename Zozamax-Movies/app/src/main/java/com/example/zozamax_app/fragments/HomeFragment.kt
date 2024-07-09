9a85da593b0f0a3c26c566d3cf62ce3d49e47c70
okituibrian <okituibrian@gmail.com>
2024-07-09 15:19:06 +0300

    fix: fixed the binding to recycler view method

--- Zozamax-Movies/app/src/main/java/com/example/zozamax_app/fragments/HomeFragment.kt
+++ Zozamax-Movies/app/src/main/java/com/example/zozamax_app/fragments/HomeFragment.kt
@@ -3,10 +3,10 @@ package com.example.zozamax_app.fragments
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
@@ -18,46 +18,51 @@ import com.example.zozamax_app.viewmodel.PopularViewModel
 import com.skydoves.transformationlayout.onTransformationStartContainer
 
 private const val TAG = "popularMovies"
-class HomeFragment : Fragment() {
 
-    class HomeFragment : Fragment() {
+class  HomeFragment : Fragment() {
 
+    private lateinit var binding: FragmentHomeBinding
+    private var _binding: FragmentHomeBinding? = null
 
-        private lateinit var binding: FragmentHomeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        onTransformationStartContainer()
+    }
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            //onTransformationStartContainer()
+    @SuppressLint("SuspiciousIndentation")
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        val repo = PopularMovieRepo()
+
+        val productViewModel: PopularViewModel by viewModels {
+            PopularModelProvider(repo)
         }
 
-        @SuppressLint("SuspiciousIndentation")
-        override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?,
-        ): View {
+        productViewModel.popularMovies.observe(viewLifecycleOwner, Observer { movies ->
+            Log.d(TAG, "Popular movies -> $movies")
+            if(movies.isNotEmpty()){
+                binding.recView.apply {
 
-            binding = FragmentHomeBinding.inflate(inflater, container, false)
-
-            val repo = PopularMovieRepo()
-
-            val productViewModel: PopularViewModel by viewModels {
-                PopularModelProvider(repo)
-            }
-
-            productViewModel.popularMovies.observe(viewLifecycleOwner, Observer { movies ->
-
-                Log.d(TAG, "popular movies -> $movies")
-                if (movies.isNotEmpty()) {
-                    binding.recView.apply {
-                        layoutManager = GridLayoutManager(requireContext(), 2)
-                        adapter = MovieAdapter(movies)
-                        setHasFixedSize(true)
-                    }
+                    layoutManager = GridLayoutManager(requireContext(), 2)
+                    adapter = MovieAdapter(movies)
+                    setHasFixedSize(true)
                 }
-            })
+            }
+        })
 
-            return binding.root
-        }
+        return binding.root
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        super.onDestroyView()
+        _binding = null
     }
 }
