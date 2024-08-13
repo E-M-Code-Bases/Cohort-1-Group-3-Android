8077c1d91eb2c6cbe6c8f8cd12aa4beeaf388ea1
okituibrian <okituibrian@gmail.com>
2024-08-13 16:38:12 +0300

    fix: added the get trailer method

--- Zozamax-Movies/app/src/main/java/com/example/zozamax_app/api_services/ApiService.kt
+++ Zozamax-Movies/app/src/main/java/com/example/zozamax_app/api_services/ApiService.kt
@@ -1,9 +1,16 @@
 package com.example.zozamax_app.api_services
 
+import com.example.zozamax_app.data.FavoriteRequest
 import com.example.zozamax_app.data.MovieApiResponse
+import com.example.zozamax_app.data.TrailerApiResponse
+import com.example.zozamax_app.data.TrailerResult
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 
@@ -58,6 +65,41 @@ interface  ApiService {
         @Query("language") language: String="en-US",
         @Query("page") page: Int =1
     ): Response<MovieApiResponse>
+
+    @GET("movie/{movie_id}/videos")
+    suspend fun getTrailers(
+        @Path("movie_id") movieId: Int,
+        @Query("api_key") apiKey: String): Response<TrailerApiResponse>
+
+    @GET("account/{account_id}/favorite/movies")
+    suspend fun getFavoriteMovies(
+        @Path("account_id") accountId: String,
+        @Header("Authorization") authHeader: String,
+//        @Query("language") language: String = "en-US",
+//        @Query("sort_by") sortBy: String = "created_at.asc",
+//        @Query("page") page: Int = 1
+    ): Response<MovieApiResponse>
+
+    @GET("account/{account_id}/favorite/movies")
+    suspend fun isFavorite(
+        @Path("account_id") accountId: String,
+        @Header("Authorization") authHeader: String,
+        @Query("movie_id") movieId: Int
+    ): Response<MovieApiResponse>
+
+    @GET("movie/trailers")
+    suspend fun getTrailers(@Query("page") i: Int = 1): Response<MovieApiResponse>
+
+    @POST("account/{account_id}/favorite")
+    suspend fun markAsFavorite(
+        @Path("account_id") accountId: String,
+        @Header("Authorization") authHeader: String,
+        @Body favoriteRequest: FavoriteRequest
+    ): Response<Unit>
+
+    @GET("search/movie")
+    suspend fun getMovieByTitle(@Query("query") title: String): Response<MovieApiResponse>
+
 
 }
 
